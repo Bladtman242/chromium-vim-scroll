8207c57c5771a05b3157a9633f6c4c5da7090de2
Bladtman <sigurt.bladt@gmail.com>
2023-01-23 18:05:29 +0100

    Always use instant scrolling
    This fixes the issue where some sites scroll slowly when j/k are held
    (this appears to have happened because the site used smooth scrolling,
    which debounces frequent scroll events)
    
    To achieve this, an unseen mistake had to be fixed: The scroll{By,To}
    methods were not actually shadowing the window.scroll{By,To} methods.

--- vim-scroll.js
+++ vim-scroll.js
@@ -11,41 +11,43 @@ var watchMap = {
     },
 };
 
-var vimScroll = {
-    eventTriggersPlugin:  function(ev) {
-        return (ev.target == document.body)
-    },
-    scrollTo: function(x,y) {
-        window.scrollTo(x,y);
-    },
-    scrollBy: function(x,y) {
-        window.scrollBy(x,y);
-    },
-    //The entrypoint of the entire extension
-    keypressHandler: function(e) {
-        if(vimScroll.eventTriggersPlugin(e)) {
-            var key = String.fromCharCode(e.keyCode);
-            if(key == 'j'){
-                scrollBy(0,50)
-            } else if(key == 'k') {
-                scrollBy(0,-50)
-            } else if(key == 'l') {
-                scrollBy(50,0)
-            } else if(key == 'h') {
-                scrollBy(-50,0)
-            } else if(key == 'G') {
-                scrollTo(window.pageXOffset,document.body.scrollHeight);
-            } else if(key == 'g') {
-                if (watchMap.contains500ms('g')){
-                    scrollTo(window.pageXOffset,0);
-                    delete watchMap['g'];
-                } else {
-                    watchMap.add('g') 
-                }
+function eventTriggersPlugin(ev) {
+    return (ev.target == document.body);
+}
+
+function vimScrollTo(x,y) {
+    window.scrollTo({left: x, top: y, behavior: 'instant'});
+}
+
+function vimScrollBy(x,y) {
+    window.scrollBy({left: x, top: y, behavior: 'instant'});
+}
+
+//The entrypoint of the entire extension
+function keypressHandler(e) {
+    if(eventTriggersPlugin(e)) {
+        var key = String.fromCharCode(e.keyCode);
+        if(key == 'j'){
+            vimScrollBy(0,50);
+        } else if(key == 'k') {
+            vimScrollBy(0,-50);
+        } else if(key == 'l') {
+            vimScrollBy(50,0);
+        } else if(key == 'h') {
+            vimScrollBy(-50,0);
+        } else if(key == 'G') {
+            vimScrollTo(window.pageXOffset,document.body.scrollHeight);
+        } else if(key == 'g') {
+            if (watchMap.contains500ms('g')){
+                vimScrollTo(window.pageXOffset,0);
+                delete watchMap['g'];
+            } else {
+                watchMap.add('g');
             }
         }
     }
-};
+}
 
 //Hook up the handler. This essentially constitutes the entire extension
-document.addEventListener("keypress", vimScroll.keypressHandler)
+document.addEventListener("keypress", keypressHandler)
+console.log('kek')
